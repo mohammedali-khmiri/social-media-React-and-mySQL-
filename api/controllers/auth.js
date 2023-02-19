7636f0dacf9eb016fbc38a8cfb4e889b8d47a8fb
khemiri mohamedali <khemirimohaedali7@gmail.com>
2023-02-19 20:29:33 +0100

    logout controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -62,4 +62,25 @@ export const login = async (req, res) => {
 
 
 //LOGOUT
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+
+    const q = "SELECT * FROM users WHERE  username = ?"
+
+
+    db.query(q,[req.body.username],(err, data)=>{
+    
+        //check if user exists
+        if (err) return res.status(500).json(err)
+    
+        if(data.length) return res.status(409).json("User already exists")
+
+    
+        db.query(q,[values],(err,data)=>{
+            if (err) return res.status(500).json(err)
+            return res.status(200).json("") 
+        })
+    
+    
+        
+    })
+};
